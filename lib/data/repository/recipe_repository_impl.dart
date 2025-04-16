1e1026ca951307f53499da5ee5559af4cf4d5b34
complete0415jiyoung <complete0415@naver.com>
2025-04-17 03:45:33 +0900

    FIX: Correct method call for retrieving recipe DTOs and clean up unused import

--- lib/data/repository/recipe_repository_impl.dart
+++ lib/data/repository/recipe_repository_impl.dart
@@ -2,7 +2,6 @@ import 'package:recipe_app/data/data_source/recipe_data_source.dart';
 import 'package:recipe_app/data/mepper/recipe_mepper.dart';
 import 'package:recipe_app/domain/model/recipe/recipe.dart';
 import 'package:recipe_app/domain/roepositpry/recipe_repository.dart';
-import 'package:recipe_app/presentation/screen/search_recipes/state/filter_search_state.dart';
 
 class RecipeRepositoryImpl implements RecipeRepository {
   final RecipeDataSource _dataSource;
@@ -11,7 +10,12 @@ class RecipeRepositoryImpl implements RecipeRepository {
 
   @override
   Future<List<Recipe>> getRecipes() async {
-    final recipeDto = await _dataSource.getRecipeDto();
+    final recipeDto = await _dataSource.getRecipeDtos();
     return recipeDto.map((e) => e.toRecipe()).toList();
+  }
+
+  @override
+  Future<Recipe> getRecipe(int recipeId) {
+    return _dataSource.getRecipeDto(recipeId).then((value) => value.toRecipe());
   }
 }
